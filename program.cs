797f01e1acbf50a7407fe6255bd54331f22578ae
feuvrt <260897031+feuvrt@users.noreply.github.com>
2026-03-31 03:09:20 +0700

    2 commit

--- program.cs
+++ program.cs
@@ -1,26 +1,2 @@
-// using System;
-// namespace CoffeeMachineSystem {
-//     class Program
-// {
-//     static void Main()
-//     {
-//         var m = new DrinkManager();
-//         m.AddDefaults();
-
-//         while (true)
-//         {
-//             Console.WriteLine("\n1 Посмотреть меню\n2 Создать напиток\n3 Обновить напиток\n4 Удалить напиток\n0 Выход");
-
-//             switch (Console.ReadLine())
-//             {
-//                 case "1": m.ReadMenu(); break;
-//                 case "2": m.CreateDrink(); break;
-//                 case "3": m.UpdateDrink(); break;
-//                 case "4": m.DeleteDrink(); break;
-//                 case "0": return;
-//             }
-//         }
-//     }
-// }
-// }
-
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
